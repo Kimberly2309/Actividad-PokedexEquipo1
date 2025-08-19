540ce3bb1a16b110eb241822a2bfe6dc1f42c024
Kimberly2309 <kimberlyperez.2309@gmail.com>
2025-08-19 17:25:28 -0300

    los 3 archivos subidos
    Terminar de hacer el estilo

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const input = document.getElementById("pokemonInput");
+    const boton = document.getElementById("buscarBtn");
+    const resultado = document.getElementById("resultado");
+
+    boton.addEventListener("click", () => {
+        const nombre = input.value.trim().toLowerCase();
+        buscarPokemon(nombre);
+    });
+
+    async function buscarPokemon(nombre) {
+        try {
+            const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`);
+            
+            if (!respuesta.ok) {
+                throw new Error("Ups, este Pokémon no existe:(");
+            }
+
+            const data = await respuesta.json();
+            mostrarPokemon(data);
+        } catch (error) {
+            mostrarError(error.message);
+        }
+    }
+
+    function mostrarPokemon(pokemon) {
+        resultado.innerHTML = `
+            <h2>${pokemon.name.toUpperCase()} (#${pokemon.id})</h2>
+            <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+            <p><strong>Altura:</strong> ${pokemon.height / 10} m</p>
+            <p><strong>Peso:</strong> ${pokemon.weight / 10} kg</p>
+            <p><strong>Tipo:</strong> ${pokemon.types.map(t => t.type.name).join(", ")}</p>
+        `;
+    }
+
+    function mostrarError(mensaje) {
+        resultado.innerHTML = `<p style="color:red;">${mensaje}</p>`;
+    }
+});
